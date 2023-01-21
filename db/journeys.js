bcc4bf2a1360109b0a5d7d3f5bd1f47279b478fd
lthu <lhuuskon@edu.lapinamk.fi>
2023-01-21 19:51:04 +0200

    minor fixes

--- db/journeys.js
+++ db/journeys.js
@@ -54,17 +54,52 @@ const getStationById = (req, res) => {
             return console.error('Error executing query', err.stack)
         } else {
             if (result.rows.length > 0)
-                res.json(result.rows)
+                res.json(result.rows);
+            else 
+                res.status(404).end();
+        }
+    })
+}
+const getTop5ReturnStationsById = (req, res) => {
+    const query = {
+        text: 'SELECT S.station_id, S.name, count(J.return_station_id) as count FROM journeys J LEFT JOIN stations S ON J.return_station_id = S.station_id WHERE J.departure_station_id = $1 GROUP BY S.station_id, S.name ORDER BY count DESC LIMIT 6 OFFSET 0',
+        values: [req.params.id]
+    }
+    
+    db.query(query, (err, result) => {
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        } else {
+            if (result.rows.length > 0)
+                res.json(result.rows);
+            else 
+                res.status(404).end();
+        }
+    })
+}
+const getTop5DepartureStationsById = (req, res) => {
+    const query = {
+        text: 'SELECT S.name, count(J.departure_station_id) as count FROM journeys J LEFT JOIN stations S ON J.departure_station_id = S.station_id WHERE J.return_station_id = $1 GROUP BY S.name ORDER BY count DESC LIMIT 5 OFFSET 0',
+        values: [req.params.id]
+    }
+    
+    db.query(query, (err, result) => {
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        } else {
+            if (result.rows.length > 0)
+                res.json(result.rows);
             else 
                 res.status(404).end();
         }
     })
 }
 
-
 module.exports = {
     getAllJourneys: getAllJourneys,
     getJourneyById: getJourneyById,
     getStationById: getStationById,
+    getTop5ReturnStationsById: getTop5ReturnStationsById,
+    getTop5DepartureStationsById: getTop5DepartureStationsById,
     getAllStations: getAllStations,
 }
--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const query = require('./db/journeys.js');
 const app = express();
+
 app.use(bodyParser.json());
 
 const port = 3000;
+
 
 
 app.get("/api/journeys", query.getAllJourneys)
 app.get("/api/journeys/:id", query.getJourneyById)
 app.get("/api/stations/:id", query.getStationById)
 app.get("/api/stations/", query.getAllStations)
+app.get("/api/stations/top5ReturnStations/:id", query.getTop5ReturnStationsById)
+app.get("/api/stations/top5DepartureStations/:id", query.getTop5DepartureStationsById)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
